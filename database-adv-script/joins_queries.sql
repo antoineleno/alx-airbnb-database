46853a0f1c2b0d69136bedfbc5d86458a00e5807
antoineleno <lenoantoine2000@gmail.com>
2025-05-13 04:57:57 +0800

    Adding inner join, left join and full outer join queries inside joins_queries.sql

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -3,7 +3,7 @@ SELECT u.first_name, u.last_name, u.email, u.phone_number, u.role, b.start_date,
 as u INNER JOIN bookings AS b ON u.user_id=b.user_id;
 
 --Retrieve all properties and their reviews, including properties that have no reviews using a LEFT JOIN.
-SELECT p.name AS Property_Name, p.description,p.location, p.pricepernight, r.rating, r.comment FROM properties as p LEFT JOIN reviews AS r ON p.property_id=r.property_id;
+SELECT p.name AS Property_Name, p.description,p.location, p.pricepernight, r.rating, r.comment FROM properties as p LEFT JOIN reviews AS r ON p.property_id=r.property_id ORDER BY p.created_at;
 
 -- Retrieve all bookings and the respective users who made those bookings using a FULL OUT JOIN.
 SELECT u.first_name, u.last_name, u.email, u.phone_number, u.role, b.start_date, b.end_date, b.total_price, b.status FROM users
