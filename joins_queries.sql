ec999864228e94b240335da8eb4101233452977e
antoineleno <lenoantoine2000@gmail.com>
2025-05-13 04:38:59 +0800

    Adding readme content inside database-adv-script

---
+++ joins_queries.sql
@@ -0,0 +1,4 @@
+-- Retrieve all bookings and the respective users who made those bookings using an INNER JOIN.
+select u.first_name, u.last_name, u.email, u.phone_number, u.role, b.start_date, b.end_date, b.total_price, b.status from users
+as u inner join bookings as b on u.user_id=b.user_id;
+-- Retrieve all bookings and the respective users who made those bookings using an INNER JOIN.
