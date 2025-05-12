8b09d33e788402df32bd9f1873286979daceda02
antoineleno <lenoantoine2000@gmail.com>
2025-05-13 05:18:30 +0800

    adding queries inside subqueries.sql

--- database-adv-script/subqueries.sql
+++ database-adv-script/subqueries.sql
@@ -2,4 +2,4 @@
 SELECT * FROM properties WHERE property_id IN (SELECT property_id FROM reviews GROUP BY property_id HAVING AVG(rating) > 4.0);
 
 -- A correlated subquery to find users who have made more than 3 bookings.
-SELECT * FROM users AS u WHERE (SELECT count(*) FROM bookings AS b WHERE b.user_id=u.user_id) > 3;
+SELECT * FROM users AS u WHERE (SELECT COUNT(*) FROM bookings AS b WHERE b.user_id=u.user_id) > 3;
